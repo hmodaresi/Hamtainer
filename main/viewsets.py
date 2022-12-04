463d80045af427cb79941a0aaa9fc28798dc89fc
Hojat Modaresi Zade <modaresi.hojat@gmail.com>
2022-12-04 09:38:24 +0330

    add filter to container

--- main/viewsets.py
+++ main/viewsets.py
@@ -1,4 +1,5 @@
-from rest_framework import viewsets
+from rest_framework import viewsets, mixins
+from django_filters.rest_framework import DjangoFilterBackend
 
 from main.models import App, Container
 from main.serializers import AppSerializer, ContainerSerializer
@@ -9,6 +10,8 @@ class AppViewSet(viewsets.ModelViewSet):
     serializer_class = AppSerializer
 
 
-class ContainerViewSet(viewsets.ModelViewSet):
+class ContainerViewSet(viewsets.GenericViewSet, mixins.ListModelMixin):
     queryset = Container.objects.all()
     serializer_class = ContainerSerializer
+    filter_backends = [DjangoFilterBackend]
+    filterset_fields = ['app']
